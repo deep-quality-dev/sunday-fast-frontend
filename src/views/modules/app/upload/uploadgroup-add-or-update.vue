196494aec5e7829d92b469b0b84a1ddd91d0e218
taoz <taozui@yodinfo.com>
2019-06-05 17:06:20 +0800

    add

---
+++ src/views/modules/app/upload/uploadgroup-add-or-update.vue
@@ -0,0 +1,120 @@
+<template>
+  <el-dialog
+    :title="!dataForm.id ? '新增' : '修改'"
+    :close-on-click-modal="false"
+    :visible.sync="visible">
+    <el-form :model="dataForm" :rules="dataRule" ref="dataForm" @keyup.enter.native="dataFormSubmit()" label-width="80px">
+    <el-form-item label="文件类型" prop="groupType">
+      <el-input v-model="dataForm.groupType" placeholder="文件类型"></el-input>
+    </el-form-item>
+    <el-form-item label="分类名称" prop="groupName">
+      <el-input v-model="dataForm.groupName" placeholder="分类名称"></el-input>
+    </el-form-item>
+    <el-form-item label="分类排序(数字越小越靠前)" prop="sort">
+      <el-input v-model="dataForm.sort" placeholder="分类排序(数字越小越靠前)"></el-input>
+    </el-form-item>
+    <el-form-item label="创建时间" prop="createTime">
+      <el-input v-model="dataForm.createTime" placeholder="创建时间"></el-input>
+    </el-form-item>
+    <el-form-item label="更新时间" prop="updateTime">
+      <el-input v-model="dataForm.updateTime" placeholder="更新时间"></el-input>
+    </el-form-item>
+    </el-form>
+    <span slot="footer" class="dialog-footer">
+      <el-button @click="visible = false">取消</el-button>
+      <el-button type="primary" @click="dataFormSubmit()">确定</el-button>
+    </span>
+  </el-dialog>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        visible: false,
+        dataForm: {
+          id: 0,
+          groupType: '',
+          groupName: '',
+          sort: '',
+          createTime: '',
+          updateTime: ''
+        },
+        dataRule: {
+          groupType: [
+            { required: true, message: '文件类型不能为空', trigger: 'blur' }
+          ],
+          groupName: [
+            { required: true, message: '分类名称不能为空', trigger: 'blur' }
+          ],
+          sort: [
+            { required: true, message: '分类排序(数字越小越靠前)不能为空', trigger: 'blur' }
+          ],
+          createTime: [
+            { required: true, message: '创建时间不能为空', trigger: 'blur' }
+          ],
+          updateTime: [
+            { required: true, message: '更新时间不能为空', trigger: 'blur' }
+          ]
+        }
+      }
+    },
+    methods: {
+      init (id) {
+        this.dataForm.id = id || 0
+        this.visible = true
+        this.$nextTick(() => {
+          this.$refs['dataForm'].resetFields()
+          if (this.dataForm.id) {
+            this.$http({
+              url: this.$http.adornUrl(`/app/uploadgroup/info/${this.dataForm.id}`),
+              method: 'get',
+              params: this.$http.adornParams()
+            }).then(({data}) => {
+              if (data && data.code === 0) {
+                this.dataForm.groupType = data.uploadgroup.groupType
+                this.dataForm.groupName = data.uploadgroup.groupName
+                this.dataForm.sort = data.uploadgroup.sort
+                this.dataForm.createTime = data.uploadgroup.createTime
+                this.dataForm.updateTime = data.uploadgroup.updateTime
+              }
+            })
+          }
+        })
+      },
+      // 表单提交
+      dataFormSubmit () {
+        this.$refs['dataForm'].validate((valid) => {
+          if (valid) {
+            this.$http({
+              url: this.$http.adornUrl(`/admin/uploadgroup/${!this.dataForm.id ? 'save' : 'update'}`),
+              method: 'post',
+              data: this.$http.adornData({
+                'id': this.dataForm.id || undefined,
+                'groupType': this.dataForm.groupType,
+                'groupName': this.dataForm.groupName,
+                'sort': this.dataForm.sort,
+                'createTime': this.dataForm.createTime,
+                'updateTime': this.dataForm.updateTime
+              })
+            }).then(({data}) => {
+              if (data && data.code === 0) {
+                this.$message({
+                  message: '操作成功',
+                  type: 'success',
+                  duration: 1500,
+                  onClose: () => {
+                    this.visible = false
+                    this.$emit('refreshDataList')
+                  }
+                })
+              } else {
+                this.$message.error(data.msg)
+              }
+            })
+          }
+        })
+      }
+    }
+  }
+</script>
