e20845ce1136f1edaf22a2539da422fa3f76cfd9
taozui <18819175397@163.com>
2019-10-17 16:04:15 +0800

    add page

---
+++ src/views/modules/app/hotelhelepmenu/hotelhelepmenu-add-or-update.vue
@@ -0,0 +1,111 @@
+<template>
+  <el-dialog
+    :title="!dataForm.id ? '新增' : '修改'"
+    :close-on-click-modal="false"
+    :visible.sync="visible">
+    <el-form :model="dataForm" :rules="dataRule" ref="dataForm" @keyup.enter.native="dataFormSubmit()" label-width="80px">
+    <el-form-item label="名称" prop="name">
+      <el-input v-model="dataForm.name" placeholder="名称"></el-input>
+    </el-form-item>
+    <el-form-item label="上级菜单" prop="pid">
+      <el-input v-model="dataForm.pid" placeholder="上级菜单"></el-input>
+    </el-form-item>
+    <el-form-item label="内容" prop="content">
+      <el-input v-model="dataForm.content" placeholder="内容"></el-input>
+    </el-form-item>
+    <el-form-item label="创建时间" prop="createtime">
+      <el-input v-model="dataForm.createtime" placeholder="创建时间"></el-input>
+    </el-form-item>
+    </el-form>
+    <span slot="footer" class="dialog-footer">
+      <el-button @click="visible = false">取消</el-button>
+      <el-button type="primary" @click="dataFormSubmit()">确定</el-button>
+    </span>
+  </el-dialog>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        visible: false,
+        dataForm: {
+          id: 0,
+          name: '',
+          pid: '',
+          content: '',
+          createtime: ''
+        },
+        dataRule: {
+          name: [
+            { required: true, message: '名称不能为空', trigger: 'blur' }
+          ],
+          pid: [
+            { required: true, message: '上级菜单不能为空', trigger: 'blur' }
+          ],
+          content: [
+            { required: true, message: '内容不能为空', trigger: 'blur' }
+          ],
+          createtime: [
+            { required: true, message: '创建时间不能为空', trigger: 'blur' }
+          ]
+        }
+      }
+    },
+    methods: {
+      init (id) {
+        this.dataForm.id = id || 0
+        this.visible = true
+        this.$nextTick(() => {
+          this.$refs['dataForm'].resetFields()
+          if (this.dataForm.id) {
+            this.$http({
+              url: this.$http.adornUrl(`/hotel/hotelhelepmenu/info/${this.dataForm.id}`),
+              method: 'get',
+              params: this.$http.adornParams()
+            }).then(({data}) => {
+              if (data && data.code === 0) {
+                this.dataForm.name = data.hotelhelepmenu.name
+                this.dataForm.pid = data.hotelhelepmenu.pid
+                this.dataForm.content = data.hotelhelepmenu.content
+                this.dataForm.createtime = data.hotelhelepmenu.createtime
+              }
+            })
+          }
+        })
+      },
+      // 表单提交
+      dataFormSubmit () {
+        this.$refs['dataForm'].validate((valid) => {
+          if (valid) {
+            this.$http({
+              url: this.$http.adornUrl(`/hotel/hotelhelepmenu/${!this.dataForm.id ? 'save' : 'update'}`),
+              method: 'post',
+              data: this.$http.adornData({
+                'id': this.dataForm.id || undefined,
+                'name': this.dataForm.name,
+                'pid': this.dataForm.pid,
+                'content': this.dataForm.content,
+                'createtime': this.dataForm.createtime
+              })
+            }).then(({data}) => {
+              if (data && data.code === 0) {
+                this.$message({
+                  message: '操作成功',
+                  type: 'success',
+                  duration: 1500,
+                  onClose: () => {
+                    this.visible = false
+                    this.$emit('refreshDataList')
+                  }
+                })
+              } else {
+                this.$message.error(data.msg)
+              }
+            })
+          }
+        })
+      }
+    }
+  }
+</script>
