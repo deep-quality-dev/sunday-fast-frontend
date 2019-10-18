f1345cc86412df43d42228b633624da9f7bde9ae
taozui <18819175397@163.com>
2019-10-18 18:09:25 +0800

    bugfix

--- src/views/modules/app/roomNum/set-room-num.vue
+++ src/views/modules/app/roomNum/set-room-num.vue
@@ -1,39 +1,58 @@
 <template>
-    <el-dialog title="批量设置房态度" :visible.sync="dialogFormVisible" :close-on-click-modal="false" :lock-scroll="true">
-        <el-form :model="form" label-width="auto">
-            <el-form-item label="房型名称">
+  <el-dialog
+    title="批量设置房态"
+    :visible.sync="dialogFormVisible"
+    :close-on-click-modal="false"
+    :lock-scroll="true"
+  >
+    <el-form :model="form" label-width="auto">
+      <el-form-item label="房型名称">
+        <el-checkbox
+          :indeterminate="isIndeterminate"
+          v-model="checkAll"
+          @change="handleCheckAllChange"
+        >全选</el-checkbox>
+        <el-row :gutter="20" v-for="item in roomData">
+          <el-col :span="8">
+            <div class="grid-content bg-purple">
+              <el-checkbox :label="item.name" :key="item.id">{{item.name}}</el-checkbox>
+            </div>
+          </el-col>
+          <el-col :span="16">
+            <div class="grid-content bg-purple">
+              <el-checkbox-group>
                 <el-checkbox
-                    :indeterminate="isIndeterminate"
-                    v-model="checkAll"
-                    @change="handleCheckAllChange"
-                >全选</el-checkbox>
-                <div style="margin: 15px 0;"></div>
-                <el-checkbox-group v-model="checkedCities" @change="handleCheckedCitiesChange">
-                    <el-checkbox v-for="city in cities" :label="city" :key="city">{{city}}</el-checkbox>
-                </el-checkbox-group>
-            </el-form-item>
-            <el-form-item label="选择日期">
-                <el-date-picker
-                    v-model="value1"
-                    type="daterange"
-                    range-separator="-"
-                    start-placeholder="开始日期"
-                    end-placeholder="结束日期"
-                ></el-date-picker>
-            </el-form-item>
-            <el-form-item label="房态状态">
-                <el-radio-group v-model="radio">
-                    <el-radio :label="3">不做改变</el-radio>
-                    <el-radio :label="6">房间开放</el-radio>
-                    <el-radio :label="9">房间关闭</el-radio>
-                </el-radio-group>
-            </el-form-item>
-        </el-form>
-        <div slot="footer" class="dialog-footer">
-            <el-button @click="dialogFormVisible = false">取 消</el-button>
-            <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
-        </div>
-    </el-dialog>
+                  v-for="money in item.hotelRoomMoney"
+                  :label="money.name"
+                  :key="money.id"
+                >{{money.name}}</el-checkbox>
+              </el-checkbox-group>
+            </div>
+          </el-col>
+        </el-row>
+      </el-form-item>
+      <el-form-item label="选择日期">
+        <el-date-picker
+          v-model="value1"
+          type="daterange"
+          range-separator="-"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+        ></el-date-picker>
+      </el-form-item>
+      <el-form-item label="房态状态">
+        <el-radio-group v-model="radio">
+          <el-radio :label="3">不做改变</el-radio>
+          <el-radio :label="6">房间开放</el-radio>
+          <el-radio :label="9">房间关闭</el-radio>
+        </el-radio-group>
+      </el-form-item>
+    </el-form>
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="dialogFormVisible = false">取 消</el-button>
+      <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+    </div>
+  </el-dialog>
 </template>
 <script>
 const cityOptions = ["上海", "北京", "广州", "深圳"];
@@ -46,12 +65,14 @@ export default {
       cities: cityOptions,
       isIndeterminate: true,
       form: {},
-      value1: ""
+      value1: "",
+      roomData: []
     };
   },
   methods: {
     init(id) {
       this.dialogFormVisible = true;
+      this.getRoomDatas();
     },
     handleCheckAllChange(val) {
       this.checkedCities = val ? cityOptions : [];
@@ -62,6 +83,18 @@ export default {
       this.checkAll = checkedCount === this.cities.length;
       this.isIndeterminate =
         checkedCount > 0 && checkedCount < this.cities.length;
+    },
+    getRoomDatas() {
+      this.$http({
+        url: this.$http.adornUrl(`/hotel/hotelroom/getAllRooms`),
+        method: "get"
+      }).then(({ data }) => {
+        if (data && data.code === 0) {
+          this.roomData = data.data;
+        } else {
+          this.$message.error(data.msg);
+        }
+      });
     }
   }
 };
