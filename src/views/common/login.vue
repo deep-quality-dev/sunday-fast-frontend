9babba5c0ad5d2b0b43cf5458c88cce8db689ab6
taozui <18819175397@163.com>
2019-10-29 00:36:10 +0800

    fix:找回密码

--- src/views/common/login.vue
+++ src/views/common/login.vue
@@ -67,7 +67,11 @@
                       </el-row>
                     </el-form-item>
                     <el-form-item>
-                      <el-button class="login-btn-submit" type="primary" @click="dataFormSubmit()">登录</el-button>
+                      <el-button
+                        class="login-btn-submit"
+                        type="primary"
+                        @click="dataFormSubmit()"
+                      >登录</el-button>
                     </el-form-item>
                   </el-form>
                   <el-row type="flex" justify="space-between">
@@ -75,12 +79,13 @@
                       <router-link :to="{ path: 'apply' }">商家入驻 ></router-link>
                     </el-col>
                     <el-col class="action">
-                      <el-checkbox v-model="memory"><a>记住我</a></el-checkbox>
+                      <el-checkbox v-model="memory">
+                        <a>记住我</a>
+                      </el-checkbox>
                       <a class="find" @click="status = 1">忘记密码</a>
                     </el-col>
                   </el-row>
                 </template>
-
                 <template v-if="status === 1">
                   <el-form
                     :model="findForm"
@@ -90,17 +95,27 @@
                     status-icon
                   >
                     <el-form-item prop="mobile">
-                      <el-input v-model="findForm.mobile" placeholder="手机号">
+                      <el-input v-model="findForm.mobile" placeholder="手机号"></el-input>
+                    </el-form-item>
+                    <el-form-item prop="captcha">
+                      <el-row :gutter="20">
+                        <el-col :span="14">
+                          <el-input v-model="findForm.captcha" placeholder="验证码"></el-input>
+                        </el-col>
+                        <el-col :span="10" class="login-captcha">
+                          <img :src="captchaPath" @click="getCaptcha()" alt="">
+                        </el-col>
+                      </el-row>
+                    </el-form-item>
+                    <el-form-item prop="code">
+                      <el-input v-model="findForm.code" placeholder="请输入验证码">
                         <template slot="append">
                           <el-button
-                            :disabled="codeCount > 0"
+                            :disabled="codeCount > 0 || !findForm.captcha"
                             @click="send"
                           >{{ codeCount !== 0 ? codeCount + '秒' : '发送验证码' }}</el-button>
                         </template>
                       </el-input>
-                    </el-form-item>
-                    <el-form-item prop="code">
-                      <el-input v-model="findForm.code" placeholder="手机验证码"></el-input>
                     </el-form-item>
                     <el-form-item prop="newPassword">
                       <el-input
@@ -201,7 +216,7 @@
 <script>
 import Footer from "./footer";
 import { getUUID } from "@/utils";
-import axios from 'axios';
+import axios from "axios";
 
 export default {
   data() {
@@ -218,9 +233,7 @@ export default {
         userName: [
           { required: true, message: "帐号不能为空", trigger: "blur" }
         ],
-        password: [
-          { required: true, message: "不能为空", trigger: "blur" }
-        ],
+        password: [{ required: true, message: "不能为空", trigger: "blur" }],
         captcha: [
           {
             required: true,
@@ -230,22 +243,19 @@ export default {
         ]
       },
       findForm: {
-        mobile: '',
-        code: '',
-        password: ''
+        mobile: "",
+        code: "",
+        password: ""
       },
       findRule: {
-        mobile: [
-          { required: true, message: "不能为空", trigger: "blur" }
-        ],
-        code: [
-          { required: true, message: "不能为空", trigger: "blur" }
-        ],
+        mobile: [{ required: true, message: "不能为空", trigger: "blur" }],
+        code: [{ required: true, message: "不能为空", trigger: "blur" }],
+        captcha: [{ required: true, message: "不能为空", trigger: "blur" }],
         newPassword: [{ required: true, message: "不能为空", trigger: "blur" }]
       },
       captchaPath: "",
       memory: true,
-      status: 0,    //0是登录 1是找回
+      status: 0, //0是登录 1是找回
       codeCount: 0 //验证码倒计时
     };
   },
@@ -253,10 +263,8 @@ export default {
     Footer
   },
   watch: {
-    status: function (value) {
-      if (value === 0) {
-        this.getCaptcha();
-      }
+    status: function(value) {
+      this.getCaptcha();
     }
   },
   created() {
@@ -327,54 +335,85 @@ export default {
     },
     // 找回密码表单
     findFormSubmit() {
-      this.$refs["findForm"].validate((valid) => {
+      this.getCaptcha();
+      this.$refs["findForm"].validate(valid => {
         if (!valid) {
           return;
         }
         const { mobile, code, newPassword } = this.findForm;
-        console.log(mobile)
-        console.log(code)
-        console.log(newPassword)
+        const { uuid, captcha } = this.dataForm;
+        console.log(mobile);
+        console.log(code);
+        console.log(newPassword);
+        this.$http({
+          url: this.$http.adornUrl("/sys/user/findPwd"),
+          method: "post",
+          data: this.$http.adornData({
+            mobile,
+            code,
+            newPassword
+          })
+        }).then(({ data }) => {
+          if (data && data.code === 0) {
+            this.$message({
+              message: "密码设置成功",
+              type: "success"
+            });
+            this.$refs["findForm"].resetFields();
+            this.status = 0;
+          } else {
+            this.getCaptcha();
+            this.$message.error(data.msg);
+          }
+        });
       });
     },
     // 发送验证码
     send() {
-      const { mobile } = this.findForm;
-
+      const { mobile, captcha } = this.findForm;
+      const { uuid } = this.dataForm;
       if (!mobile) {
         return this.$message({
-          type: 'error',
-          message: '请输入手机号码'
+          type: "error",
+          message: "请输入手机号码"
         });
       }
       if (mobile.length !== 11) {
         return this.$message({
-          type: 'error',
-          message: '手机号码输入有误'
+          type: "error",
+          message: "手机号码输入有误"
         });
       }
 
-      axios.get(`https://hotelapi.xqtinfo.cn/hotel/common/sendSmsCode?mobile=${mobile}`).then(() => {
-        this.$message({
-          message: '发送成功',
-          type: 'success'
-        });
+      this.$http({
+        url: this.$http.adornUrl("/hotel/common/smsCode"),
+        method: "get",
+        params: this.$http.adornParams({
+          mobile: mobile,
+          uuid: uuid,
+          captchaCode: captcha
+        })
+      }).then(({ data }) => {
+        if (data && data.code === 0) {
+          this.$message({
+            message: "发送成功",
+            type: "success"
+          });
 
-        let timer = null;
-        this.codeCount = 60;
-        this.$nextTick(() => {
-          timer = setInterval(() => {
-            if (this.codeCount === 0) {
-              return clearInterval(timer);
-            }
-            this.codeCount = this.codeCount - 1;
-          }, 1000);
-        });
-      }).catch((msg) => {
-        this.$message({
-          message: msg || '发送失败',
-          type: 'error'
-        });
+          let timer = null;
+          this.codeCount = 60;
+          this.$nextTick(() => {
+            timer = setInterval(() => {
+              if (this.codeCount === 0) {
+                return clearInterval(timer);
+              }
+              this.codeCount = this.codeCount - 1;
+            }, 1000);
+          });
+        } else {
+          this.$message.error(data.msg);
+          this.getCaptcha();
+        }
       });
     }
   }
@@ -382,222 +421,222 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .login-page__container {
-    width: 100%;
+.login-page__container {
+  width: 100%;
+}
+.container {
+  width: 1200px;
+  margin: 0 auto;
+}
+.carousel-container {
+  position: relative;
+  .login-container__session {
+    position: absolute;
+    // width: 1200px;
+    z-index: 100;
+    height: 100%;
+    top: 0;
+    left: 60%;
+    transform: translateX(-50%);
+    background: transparent;
   }
-  .container {
-    width: 1200px;
-    margin: 0 auto;
-  }
-  .carousel-container {
-    position: relative;
-    .login-container__session {
-      position: absolute;
-      // width: 1200px;
-      z-index: 100;
+  .login-container__box {
+    position: absolute;
+    width: 400px;
+    //height: 330px;
+    z-index: 100;
+    top: 75px;
+    right: 0;
+    padding: 5px;
+    border-radius: 8px;
+    box-sizing: border-box;
+    background-color: rgba(0, 0, 0, 0.7);
+
+    .login-main {
+      width: 100%;
       height: 100%;
-      top: 0;
-      left: 50%;
-      transform: translateX(-50%);
-      background: transparent;
-    }
-    .login-container__box {
-      position: absolute;
-      width: 400px;
-      height: 330px;
-      z-index: 100;
-      top: 75px;
-      right: 0;
-      padding: 5px;
       border-radius: 8px;
-      box-sizing: border-box;
-      background-color: rgba(0, 0, 0, 0.7);
+      // background-color: #ffffff;
 
-      .login-main {
-        width: 100%;
-        height: 100%;
-        border-radius: 8px;
-        // background-color: #ffffff;
-
-        .login-mian__title {
-          width: 80%;
-          margin: 0 auto;
-          font-size: 18px;
-          box-sizing: border-box;
+      .login-mian__title {
+        width: 80%;
+        margin: 0 auto;
+        font-size: 18px;
+        box-sizing: border-box;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        margin-bottom: 10px;
+        color: rgba(255, 255, 255, 0.6);
+        .item {
+          padding: 18px 0;
+          flex: 1;
+          max-width: 50%;
           display: flex;
           align-items: center;
-          justify-content: space-between;
-          margin-bottom: 10px;
-          color: rgba(255, 255, 255, 0.6);
-          .item {
-            padding: 18px 0;
-            flex: 1;
-            max-width: 50%;
-            display: flex;
-            align-items: center;
-            justify-content: flex-start;
-            cursor: pointer;
-            position: relative;
+          justify-content: flex-start;
+          cursor: pointer;
+          position: relative;
 
-            &.active {
-              // color: #288fe7;
-              // &::after {
-              //   content: "";
-              //   position: absolute;
-              //   bottom: 0;
-              //   left: 0;
-              //   width: 100%;
-              //   height: 3px;
-              //   background-color: #288fe7;
-              // }
-            }
+          &.active {
+            // color: #288fe7;
+            // &::after {
+            //   content: "";
+            //   position: absolute;
+            //   bottom: 0;
+            //   left: 0;
+            //   width: 100%;
+            //   height: 3px;
+            //   background-color: #288fe7;
+            // }
           }
-        }
-
-        .longi-mina__form {
-          width: 80%;
-          margin: 0 auto;
-        }
-
-        .login-captcha {
-          img {
-            max-width: 100%;
-          }
-        }
-
-        .login-btn-submit {
-          width: 100%;
         }
       }
-    }
-  }
-  .login-page__content {
-    padding: 20px 0;
-    box-sizing: border-box;
-    width: 100%;
-    background-color: #f2f2f2;
 
-    .content-card__container {
-      min-height: 300px;
-      margin: 0 auto;
-      background-color: #ffffff;
+      .longi-mina__form {
+        width: 80%;
+        margin: 0 auto;
+      }
 
-      .bottom-session {
+      .login-captcha {
+        img {
+          max-width: 100%;
+        }
+      }
+
+      .login-btn-submit {
         width: 100%;
-        .bottom-session__header {
-          // height: 55px;
-          width: 100%;
-          border-bottom: 1px solid #eeeeee;
-        }
-        .bottom-session__body {
+      }
+    }
+  }
+}
+.login-page__content {
+  padding: 20px 0;
+  box-sizing: border-box;
+  width: 100%;
+  background-color: #f2f2f2;
+
+  .content-card__container {
+    min-height: 300px;
+    margin: 0 auto;
+    background-color: #ffffff;
+
+    .bottom-session {
+      width: 100%;
+      .bottom-session__header {
+        // height: 55px;
+        width: 100%;
+        border-bottom: 1px solid #eeeeee;
+      }
+      .bottom-session__body {
+        display: flex;
+        justify-content: space-between;
+        align-items: stretch;
+        padding: 20px 0;
+        .flex-box__item {
+          flex: 1;
           display: flex;
-          justify-content: space-between;
-          align-items: stretch;
-          padding: 20px 0;
-          .flex-box__item {
-            flex: 1;
+          flex-direction: column;
+          align-items: center;
+          padding: 0 20px;
+          box-sizing: border-box;
+
+          .bd {
+            margin: 15px 0;
+            border: 1px solid #eeeeee;
+            padding: 8px;
+          }
+
+          &:not(:first-child) {
+            border-left: 1px solid #eeeeee;
+          }
+
+          .ft {
+            text-align: center;
+          }
+
+          .color-bg__blue {
+            width: 85%;
+            margin: 0 auto;
+            margin-bottom: 15px;
+            height: 40px;
+            border-left: 1px solid #ddd;
+            line-height: 40px;
+            font-size: 20px;
+            color: #fff;
+            text-align: center;
+            background-color: #288fe7;
+          }
+
+          .flex-box__server {
+            width: 85%;
+            margin: 0 auto;
             display: flex;
-            flex-direction: column;
-            align-items: center;
-            padding: 0 20px;
-            box-sizing: border-box;
+            flex-wrap: wrap;
 
-            .bd {
-              margin: 15px 0;
-              border: 1px solid #eeeeee;
-              padding: 8px;
-            }
-
-            &:not(:first-child) {
-              border-left: 1px solid #eeeeee;
-            }
-
-            .ft {
-              text-align: center;
-            }
-
-            .color-bg__blue {
-              width: 85%;
-              margin: 0 auto;
-              margin-bottom: 15px;
-              height: 40px;
-              border-left: 1px solid #ddd;
-              line-height: 40px;
-              font-size: 20px;
-              color: #fff;
-              text-align: center;
-              background-color: #288fe7;
-            }
-
-            .flex-box__server {
-              width: 85%;
-              margin: 0 auto;
-              display: flex;
-              flex-wrap: wrap;
-
-              .item {
-                width: 50%;
-              }
+            .item {
+              width: 50%;
             }
           }
         }
       }
     }
-
-    .bootom-card__container {
-      margin: 10px auto;
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-
-      .border {
-        height: 1px;
-        flex: 1;
-        background-color: #dddddd;
-      }
-
-      .text {
-        margin: 0 30px;
-      }
-    }
   }
 
-  .footer-container {
-    width: 100%;
-    min-height: 45px;
+  .bootom-card__container {
+    margin: 10px auto;
     display: flex;
     align-items: center;
-    justify-content: center;
+    justify-content: space-between;
 
-    .popyright {
-      color: #999999;
+    .border {
+      height: 1px;
+      flex: 1;
+      background-color: #dddddd;
+    }
+
+    .text {
+      margin: 0 30px;
     }
   }
+}
 
-  .nav {
-    list-style: none;
-    padding: 0;
-    margin: 0;
-    display: flex;
-    font-size: 14px;
+.footer-container {
+  width: 100%;
+  min-height: 45px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-    li {
-      cursor: pointer;
-      min-width: 50px;
-      padding: 0 20px;
-      border-right: 1px solid rgba(255, 255, 255, 0.2);
-      &:first-child {
-        border-left: 1px solid rgba(255, 255, 255, 0.2);
-      }
+  .popyright {
+    color: #999999;
+  }
+}
+
+.nav {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  font-size: 14px;
+
+  li {
+    cursor: pointer;
+    min-width: 50px;
+    padding: 0 20px;
+    border-right: 1px solid rgba(255, 255, 255, 0.2);
+    &:first-child {
+      border-left: 1px solid rgba(255, 255, 255, 0.2);
     }
   }
+}
 
-  .action {
-    text-align: right;
+.action {
+  text-align: right;
 
-    .find {
-      cursor: pointer;
-      margin-left: 6px;
-    }
+  .find {
+    cursor: pointer;
+    margin-left: 6px;
   }
+}
 </style>
